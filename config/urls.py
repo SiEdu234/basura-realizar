ecef0ad8affa33289ef77aea3a342cc354ae888b
Rafael <simedua12@gmail.com>
2026-07-06 20:18:49 -0500

    Cmabios gigantes

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import render
+from django.template.exceptions import TemplateDoesNotExist
+from django.http import Http404
 
 def home(request):
     return render(request, 'index.html')
 
+def render_template(request, template_name):
+    try:
+        return render(request, template_name)
+    except TemplateDoesNotExist:
+        raise Http404("Template does not exist")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('<path:template_name>', render_template, name='render_template'),
 ]
